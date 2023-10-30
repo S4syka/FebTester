5666b6d25155c4ce645598c895445f62c46196b5
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-10-30 13:47:45 +0400

    Finished settings template

--- FebTesting/FebTesting/Settings.Designer.cs
+++ FebTesting/FebTesting/Settings.Designer.cs
@@ -31,14 +31,17 @@
             checkBox1 = new CheckBox();
             checkBox2 = new CheckBox();
             checkBox3 = new CheckBox();
-            checkedListBox1 = new CheckedListBox();
             label1 = new Label();
+            button1 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(467, 318);
+            checkBox1.Location = new Point(144, 9);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(85, 19);
             checkBox1.TabIndex = 0;
@@ -48,7 +51,7 @@
             // checkBox2
             // 
             checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(467, 343);
+            checkBox2.Location = new Point(144, 34);
             checkBox2.Name = "checkBox2";
             checkBox2.Size = new Size(183, 19);
             checkBox2.TabIndex = 1;
@@ -58,43 +61,65 @@
             // checkBox3
             // 
             checkBox3.AutoSize = true;
-            checkBox3.Location = new Point(467, 368);
+            checkBox3.Location = new Point(144, 59);
             checkBox3.Name = "checkBox3";
             checkBox3.Size = new Size(162, 19);
             checkBox3.TabIndex = 2;
             checkBox3.Text = "Do only incorrect answers";
             checkBox3.UseVisualStyleBackColor = true;
             // 
-            // checkedListBox1
-            // 
-            checkedListBox1.FormattingEnabled = true;
-            checkedListBox1.Items.AddRange(new object[] { "asdasda", "asdas", "das", "das", "das", "dasdas", "d" });
-            checkedListBox1.Location = new Point(12, 30);
-            checkedListBox1.Name = "checkedListBox1";
-            checkedListBox1.Size = new Size(226, 346);
-            checkedListBox1.TabIndex = 3;
-            // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Location = new Point(12, 9);
             label1.Name = "label1";
-            label1.Size = new Size(97, 15);
+            label1.Size = new Size(92, 15);
             label1.TabIndex = 4;
-            label1.Text = "Choose Chapters";
+            label1.Text = "Choose Chapter";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(181, 145);
+            button1.Name = "button1";
+            button1.Size = new Size(136, 49);
+            button1.TabIndex = 5;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(12, 27);
+            numericUpDown1.Maximum = new decimal(new int[] { 7, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(42, 23);
+            numericUpDown1.TabIndex = 6;
+            numericUpDown1.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 145);
+            button2.Name = "button2";
+            button2.Size = new Size(136, 49);
+            button2.TabIndex = 7;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = true;
             // 
             // Settings
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(656, 391);
+            ClientSize = new Size(329, 204);
+            Controls.Add(button2);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button1);
             Controls.Add(label1);
-            Controls.Add(checkedListBox1);
             Controls.Add(checkBox3);
             Controls.Add(checkBox2);
             Controls.Add(checkBox1);
             Name = "Settings";
             Text = "Settings";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -104,7 +129,9 @@
         private CheckBox checkBox1;
         private CheckBox checkBox2;
         private CheckBox checkBox3;
-        private CheckedListBox checkedListBox1;
         private Label label1;
+        private Button button1;
+        private NumericUpDown numericUpDown1;
+        private Button button2;
     }
 }
