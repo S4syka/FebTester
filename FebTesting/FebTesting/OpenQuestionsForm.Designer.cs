d1034164a8eddd9228f72d279ce6b5b13f4600eb
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-10-30 16:42:44 +0400

    Modified OpenQuestions form

--- FebTesting/FebTesting/OpenQuestionsForm.Designer.cs
+++ FebTesting/FebTesting/OpenQuestionsForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FebTesting
 {
-    partial class Form1
+    partial class OpenQuestionsForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,18 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button5 = new Button();
+            button4 = new Button();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            button6 = new Button();
             SuspendLayout();
             // 
-            // Form1
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button5.ImageAlign = ContentAlignment.BottomLeft;
+            button5.Location = new Point(642, 470);
+            button5.Name = "button5";
+            button5.Size = new Size(128, 23);
+            button5.TabIndex = 18;
+            button5.Text = "Closed Questions";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button4.ImageAlign = ContentAlignment.BottomLeft;
+            button4.Location = new Point(776, 470);
+            button4.Name = "button4";
+            button4.Size = new Size(101, 23);
+            button4.TabIndex = 17;
+            button4.Text = "OpenQuestions";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.Location = new Point(13, 10);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(864, 164);
+            textBox2.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Bottom;
+            textBox1.Location = new Point(415, 470);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(65, 23);
+            textBox1.TabIndex = 15;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button3.ImageAlign = ContentAlignment.BottomLeft;
+            button3.Location = new Point(12, 470);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 14;
+            button3.Text = "Settings";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Bottom;
+            button2.Location = new Point(334, 470);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 13;
+            button2.Text = "<<";
+            button2.TextAlign = ContentAlignment.BottomCenter;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom;
+            button1.Location = new Point(486, 470);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 12;
+            button1.Text = ">>";
+            button1.TextAlign = ContentAlignment.BottomCenter;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox3.Location = new Point(12, 180);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(864, 274);
+            textBox3.TabIndex = 19;
+            // 
+            // button6
+            // 
+            button6.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button6.ImageAlign = ContentAlignment.BottomLeft;
+            button6.Location = new Point(173, 470);
+            button6.Name = "button6";
+            button6.Size = new Size(88, 23);
+            button6.TabIndex = 20;
+            button6.Text = "See Answer";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // TrueFalseForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(888, 503);
-            Name = "Form1";
-            Text = "Form1";
+            Controls.Add(button6);
+            Controls.Add(textBox3);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "TrueFalseForm";
+            Text = "Open Questions";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Button button5;
+        private Button button4;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox3;
+        private Button button6;
     }
 }
--- FebTesting/FebTesting/OpenQuestionsForm.cs
+++ FebTesting/FebTesting/OpenQuestionsForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FebTesting
 {
-    public partial class Form1 : Form
+    public partial class OpenQuestionsForm : Form
     {
-        public Form1()
+        public OpenQuestionsForm()
         {
             InitializeComponent();
         }
