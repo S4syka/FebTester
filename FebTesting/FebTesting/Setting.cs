65bb4b7268a9efde06160f689455ee3ce30a790f
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-10-30 14:01:47 +0400

    Added Settings class

---
+++ FebTesting/FebTesting/Setting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FebTesting;
+
+public class Setting
+{
+    public bool Randomize { get; private set; }
+
+    public int Chapter { get; private set; }
+
+    public bool OnlyIncorrectAnswers { get; private set; }
+
+    public Setting(bool randomize, int chapter, bool onlyIncorrectAnswers)
+    {
+        Randomize = randomize;
+        Chapter = chapter;
+        OnlyIncorrectAnswers = onlyIncorrectAnswers;
+    }
+}
