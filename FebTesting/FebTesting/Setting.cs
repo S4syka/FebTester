ba1d20301ed43fe9edee4c6570b103364ff99503
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-10-30 14:03:49 +0400

    Added AutomaticallyMoveIfCorrect attribute to settings

--- FebTesting/FebTesting/Setting.cs
+++ FebTesting/FebTesting/Setting.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FebTesting;
+﻿namespace FebTesting;
 
 public static class Setting
 {
-    public static bool Randomize { get; set; }
+    public static bool Randomize { get; set; } = false;
 
-    public static int Chapter { get; set; }
+    public static bool AutomaticallyMoveIfCorrect { get; set; } = true;
 
-    public static bool OnlyIncorrectAnswers { get; set; }
+    public static int Chapter { get; set; } = 1;
+
+    public static bool OnlyIncorrectAnswers { get; set; } = false; 
 }
