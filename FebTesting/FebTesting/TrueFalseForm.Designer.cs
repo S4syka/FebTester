ff18229d65d764cce269b34c18384a1d10b15798
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-10-30 16:43:01 +0400

    Modified TrueFalse form

--- FebTesting/FebTesting/TrueFalseForm.Designer.cs
+++ FebTesting/FebTesting/TrueFalseForm.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            AnsE_btn = new Button();
+            AnsTrue_btn = new Button();
             button5 = new Button();
             button4 = new Button();
             textBox2 = new TextBox();
@@ -35,17 +37,37 @@
             button3 = new Button();
             button2 = new Button();
             button1 = new Button();
-            textBox3 = new TextBox();
-            button6 = new Button();
             SuspendLayout();
+            // 
+            // AnsE_btn
+            // 
+            AnsE_btn.Anchor = AnchorStyles.Bottom;
+            AnsE_btn.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            AnsE_btn.Location = new Point(486, 345);
+            AnsE_btn.Name = "AnsE_btn";
+            AnsE_btn.Size = new Size(391, 119);
+            AnsE_btn.TabIndex = 23;
+            AnsE_btn.Text = "False";
+            AnsE_btn.UseVisualStyleBackColor = true;
+            // 
+            // AnsTrue_btn
+            // 
+            AnsTrue_btn.Anchor = AnchorStyles.Bottom;
+            AnsTrue_btn.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            AnsTrue_btn.Location = new Point(13, 345);
+            AnsTrue_btn.Name = "AnsTrue_btn";
+            AnsTrue_btn.Size = new Size(396, 119);
+            AnsTrue_btn.TabIndex = 22;
+            AnsTrue_btn.Text = "True";
+            AnsTrue_btn.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
             button5.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             button5.ImageAlign = ContentAlignment.BottomLeft;
-            button5.Location = new Point(642, 470);
+            button5.Location = new Point(669, 470);
             button5.Name = "button5";
-            button5.Size = new Size(128, 23);
+            button5.Size = new Size(101, 23);
             button5.TabIndex = 18;
             button5.Text = "Closed Questions";
             button5.UseVisualStyleBackColor = true;
@@ -67,7 +89,7 @@
             textBox2.Location = new Point(13, 10);
             textBox2.Multiline = true;
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(864, 164);
+            textBox2.Size = new Size(864, 329);
             textBox2.TabIndex = 16;
             // 
             // textBox1
@@ -111,33 +133,13 @@
             button1.TextAlign = ContentAlignment.BottomCenter;
             button1.UseVisualStyleBackColor = true;
             // 
-            // textBox3
-            // 
-            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            textBox3.Location = new Point(12, 180);
-            textBox3.Multiline = true;
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(864, 274);
-            textBox3.TabIndex = 19;
-            // 
-            // button6
-            // 
-            button6.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            button6.ImageAlign = ContentAlignment.BottomLeft;
-            button6.Location = new Point(173, 470);
-            button6.Name = "button6";
-            button6.Size = new Size(88, 23);
-            button6.TabIndex = 20;
-            button6.Text = "See Answer";
-            button6.UseVisualStyleBackColor = true;
-            // 
             // TrueFalseForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(888, 503);
-            Controls.Add(button6);
-            Controls.Add(textBox3);
+            Controls.Add(AnsE_btn);
+            Controls.Add(AnsTrue_btn);
             Controls.Add(button5);
             Controls.Add(button4);
             Controls.Add(textBox2);
@@ -146,12 +148,15 @@
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "TrueFalseForm";
-            Text = "Open Questions";
+            Text = "True False Questions";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
+
+        private Button AnsE_btn;
+        private Button AnsTrue_btn;
         private Button button5;
         private Button button4;
         private TextBox textBox2;
@@ -159,7 +164,5 @@
         private Button button3;
         private Button button2;
         private Button button1;
-        private TextBox textBox3;
-        private Button button6;
     }
 }
