22abff52384030ad9dc0862c6605ba7f91cb3c6c
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-10-30 13:31:06 +0400

    Added Save button in settings template

---
+++ FebTesting/FebTesting/Settings.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace FebTesting
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkedListBox1 = new CheckedListBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(467, 318);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(85, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Randomize";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(467, 343);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(183, 19);
+            checkBox2.TabIndex = 1;
+            checkBox2.Text = "Automatically move if correct";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(467, 368);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(162, 19);
+            checkBox3.TabIndex = 2;
+            checkBox3.Text = "Do only incorrect answers";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "asdasda", "asdas", "das", "das", "das", "dasdas", "d" });
+            checkedListBox1.Location = new Point(12, 30);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(226, 346);
+            checkedListBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Choose Chapters";
+            // 
+            // Settings
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(656, 391);
+            Controls.Add(label1);
+            Controls.Add(checkedListBox1);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Name = "Settings";
+            Text = "Settings";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckedListBox checkedListBox1;
+        private Label label1;
+    }
+}
